376122dea51042b7c0c4ed8c8f35f6901648da36
Amin Abbaspour <amin.abbaspour@okta.com>
2025-09-15 14:01:27 +1000

    naked -> www

--- tf/dns.tf
+++ tf/dns.tf
@@ -3,6 +3,46 @@ locals {
   placeholder_ipv6 = "100::"     # placeholder IPv6 so traffic goes to worker routes
 }
 
+# Apex (root) domain records - required for redirecting replate.dev -> www.replate.dev
+resource "cloudflare_dns_record" "root_a" {
+  zone_id = data.cloudflare_zone.replate-dev.zone_id
+  name    = "@"
+  content = local.placeholder_ipv4
+  type    = "A"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "root_aaaa" {
+  zone_id = data.cloudflare_zone.replate-dev.zone_id
+  name    = "@"
+  content = local.placeholder_ipv6
+  type    = "AAAA"
+  proxied = true
+  ttl     = 1
+}
+
+# WWW subdomain records (target for the redirect)
+/*
+resource "cloudflare_dns_record" "www_a" {
+  zone_id = data.cloudflare_zone.replate-dev.zone_id
+  name    = "www"
+  content = local.placeholder_ipv4
+  type    = "A"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "www_aaaa" {
+  zone_id = data.cloudflare_zone.replate-dev.zone_id
+  name    = "www"
+  content = local.placeholder_ipv6
+  type    = "AAAA"
+  proxied = true
+  ttl     = 1
+}
+*/
+
 # IPv4 - A
 resource "cloudflare_dns_record" "donor" {
   zone_id = data.cloudflare_zone.replate-dev.zone_id
@@ -57,4 +97,19 @@ resource "cloudflare_dns_record" "adminV6" {
   type    = "AAAA"
   proxied = true
   ttl     = 1
+}
+
+# Redirect apex to www using Cloudflare Page Rule (301)
+resource "cloudflare_page_rule" "root_to_www" {
+  zone_id  = data.cloudflare_zone.replate-dev.zone_id
+  target   = "${var.top_level_domain}/*"
+  priority = 1
+
+  status   = "active"
+  actions = {
+    forwarding_url = {
+      url         = "https://www.${var.top_level_domain}/$1"
+      status_code = 301
+    }
+  }
 }
