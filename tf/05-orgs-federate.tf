6c1719aaa7efba7cce23b5e89746a35d2580a31f
Amin Abbaspour <amin.abbaspour@okta.com>
2025-10-31 13:13:58 +1100

    fed step 2

--- tf/05-orgs-federate.tf
+++ tf/05-orgs-federate.tf
@@ -1,9 +1,23 @@
-resource "auth0_organization" "fed-supplier-org" {
-  name = "fed-supplier"
-  display_name = "Federated Supplier"
-  branding {
-    logo_url = "https://media.licdn.com/dms/image/v2/C560BAQEyAJ6vJjid9w/company-logo_200_200/company-logo_200_200/0/1675790205789/supplier_io_logo?e=1763596800&v=beta&t=iIYumFn6g-wqcUrS9OFlF9P1dBN2Q7vVPRLMeq39oRQ"
-  }
+## -- Okta --
+resource "okta_app_signon_policy" "only_1fa" {
+  name        = "Replate Org 1FA policy"
+  description = "Authentication Policy to be used simple apps."
+}
+
+resource "okta_app_signon_policy_rule" "only_1fa_rule" {
+  policy_id                   = okta_app_signon_policy.only_1fa.id
+  name                        = "Password only"
+  factor_mode                 = "1FA"
+  re_authentication_frequency = "PT43800H"
+  status                      = "ACTIVE"
+  constraints = [
+    jsonencode({
+      "knowledge" : {
+        "required" : false
+        "types" : ["password"]
+      }
+    })
+  ]
 }
 
 # Okta Group for Federated Supplier employees
@@ -26,6 +40,8 @@ resource "okta_app_oauth" "replate-rwa" {
 
   # OIDC settings
   issuer_mode                = "DYNAMIC"
+
+  authentication_policy = okta_app_signon_policy.only_1fa.id
 }
 
 resource "okta_app_group_assignments" "replate-rwa-group-assignment" {
@@ -35,7 +51,45 @@ resource "okta_app_group_assignments" "replate-rwa-group-assignment" {
   }
 }
 
-# Okta workforce federation connection
+# Sample users for the workforce group
+resource "okta_user" "supplier-io_admin" {
+  first_name = "Adam"
+  last_name  = "Supplier"
+  login      = "admin@supplier.io"
+  email      = "admin@supplier.io"
+  status     = "ACTIVE"
+  password = var.default-password
+}
+
+resource "okta_user" "supplier-io_member" {
+  first_name = "Maria"
+  last_name  = "Supplier"
+  login      = "member@supplier.io"
+  email      = "member@supplier.io"
+  status     = "ACTIVE"
+  password = var.default-password
+}
+
+
+# Assign users to the workforce group
+resource "okta_group_memberships" "supplier-io_members" {
+  group_id = okta_group.supplier_workforce.id
+  users = [
+    okta_user.supplier-io_admin.id,
+    okta_user.supplier-io_member.id
+  ]
+}
+
+## -- Auth0 --
+# supplier federation connection
+resource "auth0_organization" "fed-supplier-org" {
+  name = "fed-supplier"
+  display_name = "Federated Supplier"
+  branding {
+    logo_url = "https://media.licdn.com/dms/image/v2/C560BAQEyAJ6vJjid9w/company-logo_200_200/company-logo_200_200/0/1675790205789/supplier_io_logo?e=1763596800&v=beta&t=iIYumFn6g-wqcUrS9OFlF9P1dBN2Q7vVPRLMeq39oRQ"
+  }
+}
+
 resource "auth0_connection" "fed-supplier_workforce" {
   name     = "fed-supplier-workforce"
   strategy = "okta"
@@ -90,5 +144,6 @@ resource "auth0_organization_connections" "fed-supplier-connections" {
   organization_id = auth0_organization.fed-supplier-org.id
   enabled_connections {
     connection_id = auth0_connection.fed-supplier_workforce.id
+    assign_membership_on_login = true
   }
 }
