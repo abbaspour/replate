21240c3ddc3dd4ce62b4100f353ced7b9c54895e
Amin Abbaspour <amin.abbaspour@okta.com>
2025-11-10 11:22:35 +1100

    Squashed 'agent/oidc-bash/' changes from a9ede7d..d8485ec
    d8485ec Token vault fed conn (#7)
    
    git-subtree-dir: agent/oidc-bash
    git-subtree-split: d8485ec5d4f1c69d5b7a72ede2e80586ce8632c3

--- business/db/business-drop.sql
+++
@@ -1,8 +0,0 @@
--- Drop tables in reverse order of dependency to avoid foreign key constraint errors
-DROP TABLE IF EXISTS SsoInvitations;
-DROP TABLE IF EXISTS Suggestions;
-DROP TABLE IF EXISTS PickupJobs;
-DROP TABLE IF EXISTS PickupSchedules;
-DROP TABLE IF EXISTS Donations;
-DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Organizations;
